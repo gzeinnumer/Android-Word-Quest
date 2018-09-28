40ff8502303004a5d08b2d388745af7e32448034
Abdul Aris <abdularisrahmanudin10@gmail.com>
2018-09-28 16:38:19 +0700

    fix string list grid generator, row is zero based so it shouldn't be >= or <= (remove the equal sign)

--- app/src/main/java/com/aar/app/wsp/commons/generator/StringListGridGenerator.java
+++ app/src/main/java/com/aar/app/wsp/commons/generator/StringListGridGenerator.java
@@ -19,8 +19,6 @@ public class StringListGridGenerator extends GridGenerator<List<Word>, List<Word
 
     @Override
     public List<Word> setGrid(List<Word> dataInput, char[][] grid) {
-//        Util.sortByLength(dataInput);
-
         List<Word> usedWords = new ArrayList<>();
         int usedCount;
         for (int i = 0; i < MIN_GRID_ITERATION_ATTEMPT; i++) {
@@ -109,17 +107,17 @@ public class StringListGridGenerator extends GridGenerator<List<Word>, List<Word
      */
     private boolean isValidPlacement(int row, int col, Direction dir, char gridArr[][], String word) {
         int wLen = word.length();
-        if (dir == Direction.EAST && (col + wLen) >= gridArr[0].length) return false;
+        if (dir == Direction.EAST && (col + wLen) > gridArr[0].length) return false;
         if (dir == Direction.WEST && (col - wLen) < 0) return false;
 
         if (dir == Direction.NORTH && (row - wLen) < 0) return false;
-        if (dir == Direction.SOUTH && (row + wLen) >= gridArr.length) return false;
+        if (dir == Direction.SOUTH && (row + wLen) > gridArr.length) return false;
 
-        if (dir == Direction.SOUTH_EAST && ((col + wLen) >= gridArr[0].length || (row + wLen) >= gridArr.length)) return false;
+        if (dir == Direction.SOUTH_EAST && ((col + wLen) > gridArr[0].length || (row + wLen) > gridArr.length)) return false;
         if (dir == Direction.NORTH_WEST && ((col - wLen) < 0 || (row - wLen) < 0)) return false;
 
-        if (dir == Direction.SOUTH_WEST && ((col - wLen) < 0 || (row + wLen) >= gridArr.length)) return false;
-        if (dir == Direction.NORTH_EAST && ((col + wLen) >= gridArr[0].length || (row - wLen) < 0)) return false;
+        if (dir == Direction.SOUTH_WEST && ((col - wLen) < 0 || (row + wLen) > gridArr.length)) return false;
+        if (dir == Direction.NORTH_EAST && ((col + wLen) > gridArr[0].length || (row - wLen) < 0)) return false;
 
         for (int i = 0; i < wLen; i++) {
             if (gridArr[row][col] != Util.NULL_CHAR && gridArr[row][col] != word.charAt(i))
