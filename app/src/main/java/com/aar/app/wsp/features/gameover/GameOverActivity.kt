a603a7bd342232e681e242ba46196d2780741de8
Abdul Aris <abdularisrahmanudin10@gmail.com>
2020-11-05 06:28:14 +0700

    Use view binding in game over activity

--- app/src/main/java/com/aar/app/wsp/features/gameover/GameOverActivity.kt
+++ app/src/main/java/com/aar/app/wsp/features/gameover/GameOverActivity.kt
@@ -3,55 +3,75 @@ package com.aar.app.wsp.features.gameover
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
 import androidx.core.app.NavUtils
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.observe
 import com.aar.app.wsp.R
 import com.aar.app.wsp.WordSearchApp
 import com.aar.app.wsp.commons.DurationFormatter.fromInteger
+import com.aar.app.wsp.databinding.ActivityGameOverBinding
 import com.aar.app.wsp.features.FullscreenActivity
 import com.aar.app.wsp.features.gameplay.GamePlayActivity
 import com.aar.app.wsp.model.GameData
-import kotlinx.android.synthetic.main.activity_game_over.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class GameOverActivity : FullscreenActivity() {
-    @JvmField
     @Inject
-    var mViewModelFactory: ViewModelProvider.Factory? = null
+    lateinit var mViewModelFactory: ViewModelProvider.Factory
 
-    private lateinit var viewModel: GameOverViewModel
+    private val viewModel: GameOverViewModel by viewModels { mViewModelFactory }
+
+    private lateinit var binding: ActivityGameOverBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game_over)
+        binding = ActivityGameOverBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         (application as WordSearchApp).appComponent.inject(this)
-        viewModel = ViewModelProviders.of(this, mViewModelFactory).get(GameOverViewModel::class.java)
-        viewModel.onGameDataLoaded.observe(this) { gameData ->
-            gameData?.let {
-                showGameStat(it)
-            }
-        }
-        viewModel.onGameDataReset.observe(this) { gameDataId ->
-            val intent = Intent(this@GameOverActivity, GamePlayActivity::class.java)
-            intent.putExtra(GamePlayActivity.EXTRA_GAME_DATA_ID, gameDataId)
-            startActivity(intent)
-            finish()
-        }
 
-        intent.extras?.getInt(EXTRA_GAME_ROUND_ID)?.let { gameId ->
-            lifecycleScope.launch { viewModel.loadData(gameId) }
-        }
+        setupViews()
+        setupViewModel()
+        loadGameData()
+    }
 
-        main_menu_btn.setOnClickListener {
+    private fun setupViews() {
+        binding.mainMenuBtn.setOnClickListener {
             onBackPressed()
         }
-        btnReplay.setOnClickListener {
-            lifecycleScope.launch {
-                viewModel.resetCurrentGameData()
-            }
+        binding.btnReplay.setOnClickListener {
+            resetCurrentGameData()
+        }
+    }
+
+    private fun setupViewModel() {
+        viewModel.onGameDataLoaded.observe(this) { gameData ->
+            gameData?.let { showGameStat(it) }
+        }
+        viewModel.onGameDataReset.observe(this) { gameDataId ->
+            goToGamePlay(gameDataId)
+        }
+    }
+
+    private fun goToGamePlay(gameDataId: Int) {
+        val intent = Intent(this@GameOverActivity, GamePlayActivity::class.java)
+        intent.putExtra(GamePlayActivity.EXTRA_GAME_DATA_ID, gameDataId)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun resetCurrentGameData() {
+        lifecycleScope.launch {
+            viewModel.resetCurrentGameData()
+        }
+    }
+
+    private fun loadGameData() {
+        intent.extras?.getInt(EXTRA_GAME_ROUND_ID)?.let { gameId ->
+            lifecycleScope.launch { viewModel.loadData(gameId) }
         }
     }
 
@@ -70,17 +90,17 @@ class GameOverActivity : FullscreenActivity() {
 
     private fun showGameStat(gd: GameData) {
         if (gd.isGameOver) {
-            textCongrat.setText(R.string.lbl_game_over)
-            game_stat_text.visibility = View.GONE
+            binding.textCongrat.setText(R.string.lbl_game_over)
+            binding.gameStatText.visibility = View.GONE
         } else {
             val strGridSize = gd.grid!!.rowCount.toString() + " x " + gd.grid!!.colCount
             var str = getString(R.string.finish_text)
             str = str.replace(":gridSize".toRegex(), strGridSize)
             str = str.replace(":uwCount".toRegex(), gd.usedWords.size.toString())
             str = str.replace(":duration".toRegex(), fromInteger(gd.duration))
-            textCongrat.setText(R.string.congratulations)
-            game_stat_text.visibility = View.VISIBLE
-            game_stat_text.text = str
+            binding.textCongrat.setText(R.string.congratulations)
+            binding.gameStatText.visibility = View.VISIBLE
+            binding.gameStatText.text = str
         }
     }
 
